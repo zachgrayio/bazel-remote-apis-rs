330479e317a3af18f541a28142db1e414bfa68fa
Zach Gray <zach.gray@bitrise.io>
2023-04-12 22:28:25 -0600

    server

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use tonic_build;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().build_server(false).compile(
+    tonic_build::configure().build_server(true).compile(
         &[
             "proto/bazel-remote-apis/build/bazel/remote/execution/v2/remote_execution.proto",
             "proto/googleapis/google/bytestream/bytestream.proto",

6956bc716252d097317653449d9fde4d6911dbab
Zach Gray <zach@zachgray.io>
2024-06-05 11:42:33 -0700

    include descriptor

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 use tonic_build;
+use std::env;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().build_server(true).compile(
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    tonic_build::configure()
+        .build_server(true)
+        .file_descriptor_set_path(out_dir.join("reapi_descriptor.bin"))
+        .compile(
         &[
             "proto/bazel-remote-apis/build/bazel/remote/execution/v2/remote_execution.proto",
             "proto/googleapis/google/bytestream/bytestream.proto",

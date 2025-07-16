4826101b0115a005efc9d346bdef454604bdcd72
Zach Gray <zach@zachgray.io>
2025-07-16 04:29:38 -0700

    use bytes

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ use std::path::PathBuf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_build::configure()
+        .bytes(&["."])
         .build_server(true)
         .file_descriptor_set_path(out_dir.join("reapi_descriptor.bin"))
         .compile_protos(

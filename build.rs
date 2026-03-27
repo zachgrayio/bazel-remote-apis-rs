48751f97c60d7ed2b0afe5b3179c03e57b4aae0c
Zach Gray <zach@zachgray.io>
2026-03-27 13:39:19 -0700

    bump deps and edition

--- build.rs
+++ build.rs
@@ -1,11 +1,10 @@
-use tonic_build;
 use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    tonic_build::configure()
-        .bytes(&["."])
+    tonic_prost_build::configure()
+        .bytes(".")
         .build_server(true)
         .file_descriptor_set_path(out_dir.join("reapi_descriptor.bin"))
         .compile_protos(
